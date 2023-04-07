c49c5777eb1fc13b1b5fb08220d7ab6e8d1c7646
nadavoved <nadavove@gmail.com>
2023-04-07 18:29:44 +0300

    updated pwsh setup

--- update_assets/pwsh_setup.ps1
+++ update_assets/pwsh_setup.ps1
@@ -3,35 +3,42 @@ $browser = 'C:\Program Files (x86)\Microsoft\Edge\'`
 
 $eng = 'https://www.bing.com'
 
-$project = 'C:\Users\Nadav\Documents\ALL_PROJECTS\smapy'
+$project = '~\Documents\ALL_PROJECTS\smapy'
 
 $server = 'http://localhost:3002/'
+
 $figma_url = 'https://www.figma.com/file/'`
 + 'sAvmr55UwcNhZNAJ9XGpA6/smapy-design?node-id=294%3A36462&t=vWrvHj8eaAd2tj3q-1'
-$shell = "C:\Users\Nadav\AppData\Local\Microsoft\WindowsApps\"`
-+ "Microsoft.PowerShell_8wekyb3d8bbwe\pwsh.exe"
 
 function goto {
-	param($text, [Alias('s')] [switch] $search)
-	
-	if ($search) {
-		& $browser $eng/search?q=$text
-	}
+    param($text, [Alias('s')] [switch] $search)
 
-	else {
-		& $browser $text
-	}
+    if ($search) {
+        & $browser $eng/search?q=$text
+    }
+
+    else {
+        & $browser $text
+    }
 }
 
 function run_smapy {
-	param([Alias('l')] $lang = 'he')
-	python zz_updator.py
-	goto $server/$lang
-	goto $figma_url
-	Set-Location $project/app
-	wt -w 0 sp $shell -NoExit -c 'cd $project/app && yarn storybook'
-	yarn dev
-	 
+    param([Alias('l')] $lang = 'he')
+    
+    Set-Location $project/app
+    Start-Job -ScriptBlock {yarn dev} -Name 'dev'
+
+    Set-Location $project
+    python extract_he_and_en.py
+    python zz_updator.py
+
+    goto $server/$lang
+    goto $figma_url
+
+    Set-Location $project/app
+    Start-Job -ScriptBlock {yarn storybook} -Name 'storybook'
+
+    
 }
 
 
