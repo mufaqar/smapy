431a12fde398cc6e4e411a23ae4c375b039e1656
Mufaqar <mufaqar@gmail.com>
2023-04-20 18:10:05 +0500

    user prfile

--- app/src/components/auth/user-id.tsx
+++ app/src/components/auth/user-id.tsx
@@ -8,6 +8,10 @@ import {
 } from "../common/forms/usePrepareSchema";
 import type { CustomerRole, LoginValues } from "./user-auth-schema";
 import { schemaLogin } from "./user-auth-schema";
+import PageBanner from "../landing-page/page-banner";
+import Image from "next/image";
+import Link from "next/link";
+import { Footer } from "../layout/footer";
 
 const schema = schemaLogin;
 
@@ -28,35 +32,62 @@ export const UserId = ({ onSubmit, role }: Props) => {
 
   console.log(`muly:UserID`, { formContext });
   return (
-    <div className="flex flex-row items-center">
-      <img
-        src="/images/forms/login.svg"
-        width={364}
-        className="row-span-3"
-        alt=""
-      />
-      <div className="flex flex-col items-start">
-        <h3 className="text-4xl font-light">
-          {(text && text[`entry_${role}`]) || null}
-        </h3>
-        <h1 className="text-5xl font-normal">
-          {evaluateFormControlCallback(label)}
-        </h1>
+    <>
+      <section
+        className="flex h-[450px] flex-col items-center justify-start bg-cover bg-right-bottom bg-no-repeat px-4 "
+        style={{
+          backgroundImage: `url("/images/rectangle-26.png")`,
+        }}
+      >
+        <div className="container mx-auto pt-10">
+          <Link href="/">
+            <Image
+              src="/images/mobile-logo.svg"
+              width={250}
+              height={100}
+              alt="logo"
+            />
+          </Link>
+        </div>
+      </section>
+      <section className="p-8" >
+      <div className="flex flex-col md:flex-row bg-white shadow rounded-3xl gap-20 -mt-[300px] mx-auto max-w-[1020px] p-10 lg:p-20 items-center">
+        <img
+          src="/images/forms/login.svg"
+          width={364}
+          className="row-span-3"
+          alt=""
+        />
+        <div className="flex flex-col items-start">
+          <h3 className="text-2xl font-light">
+            {(text && text[`entry_${role}`]) || null}
+          </h3>
+          <h1 className="text-4xl font-semibold mb-8">
+            {evaluateFormControlCallback(label)}
+          </h1>
 
-        <Form
-          formContext={formContext}
-          schema={schema}
-          // eslint-disable-next-line @typescript-eslint/no-misused-promises
-          onSubmit={onSubmit}
-          props={{}}
-          formProps={{
-            submit: {
-              notification: false,
-              text: t('user_id.next') || '',
-            },
-          }}
-        ></Form>
+          <Form
+            formContext={formContext}
+            schema={schema}
+            // eslint-disable-next-line @typescript-eslint/no-misused-promises
+            onSubmit={onSubmit}
+            props={{}}
+            formProps={{
+              submit: {
+                notification: false,
+                text: t("user_id.next") || "",
+              },
+            }}
+          ></Form>
+        </div>
       </div>
-    </div>
+      </section>
+      <div className="relative mt-80 md:mt-20">
+        <Image src="/images/shape/bg-r-design.svg" className="absolute -top-40" width={130} height={100} alt="shape"/>
+        <Image src="/images/faq-shape.svg" className="absolute left-0 -top-60" width={130} height={100} alt="shape"/>
+      </div>
+     <Footer />
+
+    </>
   );
 };
